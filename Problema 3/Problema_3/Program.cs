a720a119bfbd7758adac6a76147253a32aea11d5
JOSE CARLOS AVALOS PEREZ <jcavalosp@correo.url.edu.gt>
2022-11-05 15:44:07 -0600

    Problema 3

---
+++ Problema 3/Problema_3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = "";
+            Console.WriteLine("Ingrese un numero factorial con la siguiente estructura 'n!' \n");
+            num = Console.ReadLine();
+            int l = 0;
+            string letra = num.Substring(num.Length - 1);
+
+            
+            if (letra == "!")
+            {
+                letra = num.Substring(0, num.Length - 1);
+                l = Convert.ToInt32(letra);
+                Console.WriteLine("El resultado del factorial es: " + factorial(Convert.ToInt32(l)));
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Al numero factorial le hace falta '!' en su escritura");
+                Console.ReadLine();
+            }
+            Console.ReadKey();
+        }
+
+        static int factorial(int n)
+        {
+            int resultado = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                resultado = resultado * i;
+            }
+            return resultado;
+        }
+    }
+}
